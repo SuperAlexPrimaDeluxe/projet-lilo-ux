84e062f23bc31ec6e53ade960d9e77ac219c987b
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-02 17:11:21 +0100

    Test correction js

--- js/pathcorrecter.js
+++ js/pathcorrecter.js
@@ -1,6 +1,6 @@
 /* Appel des îcones et img HEADER */
 
-document.getElementById("logoheader").src = "/medias/icons/lilo_logo.svg";
+document.getElementById("logoheader").src = "../medias/icons/lilo_logo.svg";
 document.getElementById("icon1").src = "/medias/icons/poeple2.svg"; 
 document.getElementById("icon2").src = "/medias/icons/basket.svg"; 
 document.getElementById("icon3").src = "/medias/icons/coeur_lilo.svg"; 
