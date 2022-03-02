033f8a1b60c0729439931ff4b9885020df207c56
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-02 17:27:43 +0100

    Sutie d

--- js/pathcorrecter.js
+++ js/pathcorrecter.js
@@ -1,7 +1,7 @@
 /* Appel des îcones et img HEADER */
 
-document.getElementById("logoheader").src = "./medias/icons/lilo_logo.svg";
-document.getElementById("icon1").src = "../medias/icons/poeple2.svg"; 
+document.getElementById("logoheader").src = "projet-lilo-ux/medias/icons/lilo_logo.svg";
+document.getElementById("icon1").src = "/projet-lilo-ux/medias/icons/lilo_logo.svg"; 
 document.getElementById("icon2").src = ".../medias/icons/basket.svg"; 
 document.getElementById("icon3").src = "medias/icons/coeur_lilo.svg"; 
 document.getElementById("burgerheader").src = "/medias/icons/burger.svg"; 
