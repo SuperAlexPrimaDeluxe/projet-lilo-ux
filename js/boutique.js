c375d38b0c8a224c81410aed2fcbfaf5cf2b2c55
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-03 11:52:10 +0100

    Ajout de la page badge dans sa version 1

---
+++ js/boutique.js
@@ -0,0 +1,24 @@
+var bruno = document.querySelectorAll('.btnalex');
+
+console.log(bruno);
+console.log("SOSOSOS");
+
+
+
+
+
+
+// const buttonsContainer = document.getElementById("buttonsContainer");
+
+// for (var i = 0; i < 5; i++) {
+//   const button = document.createElement("button");
+//   button.innerText = i;
+//   button.addEventListener("click", function() {
+//     console.log(i)
+//   })
+//   buttonsContainer.appendChild(button);
+
+
+
+
+
