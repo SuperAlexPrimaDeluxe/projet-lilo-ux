6b43b9d5885246465724466962385f2261cb54c5
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-03 20:44:42 +0100

    Ajout de du systèmes de filtre boutique pt 1

--- js/boutique.js
+++ js/boutique.js
@@ -1,3 +1,26 @@
+    //Attempt to get the element using document.getElementById
+    var badges = document.getElementById("bannertxt");
+    console.log(badges.textContent);
+    
+    if (badges.textContent.includes('BADGES')) { 
+      alert("OK");
+      document.getElementById("clicbold1").style.borderRight ="7px solid #7A7A7A";
+      document.getElementById("clicbold1").style.borderBottom ="7px solid #7A7A7A";
+      document.getElementById("clicbold1").style.borderTop ="2px solid #7A7A7A";
+      document.getElementById("clicbold1").style.borderLeft ="2px solid #7A7A7A";
+      document.getElementById("clicbold1").style.fontSize ="18px";
+      document.getElementById("clicbold1").style.fontWeight ="bold";
+     }else{
+       alert("FUCKO");
+     }
+ 
+ 
+ 
+
+
+
+
+// fonction pour le clic sur le bouton like
 function myFunction() {
   var elements = document.getElementsByClassName("fig2");
 
