fa2e406b76207cf3733d2229e17526b795403beb
Nefercricri <95749140+Nefercricri@users.noreply.github.com>
2022-03-05 22:47:52 +0100

    Pages-connexion, dashboard
    Update et responsive css, html, js

---
+++ js/dashboard.js
@@ -0,0 +1,9 @@
+
+function toggle_text() {
+var span = document.getElementById("span_txt");
+if(span.style.display == "none") {
+    span.style.display = "block";
+} else {
+    span.style.display = "none";
+}
+} 
