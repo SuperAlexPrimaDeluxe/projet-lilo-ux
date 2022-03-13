3cd6e5f3d555f632db13b18a484c73070cdb49e0
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-13 14:41:20 +0100

    Suite correction badges
    FILTRES

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-import Header from './templates/header';
-import Footer from './templates/footer';
-customElements.define('main-header', Header);
-customElements.define('main-footer', Footer);
+const actionButton = document.getElementById("showhideart1");
+const itemList = document.getElementsByClassName('article').style.display='none';
+itemList.style.display="none";
+
+const toggleShowList = () => {
+  const isHidden = itemList.style.display === "none";
+  if (isHidden) {
+    // Display hidden element
+    itemList.style.display = "block";
+    
+  } else {
+    // Hide element
+    itemList.style.display = "none";
+  }
+};
+
+// Adding click event listener
+actionButton.addEventListener("click", toggleShowList);
+
+
+
+
 
