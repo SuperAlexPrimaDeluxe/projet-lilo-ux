214a3808b460cc44a39817ae080e0a9d43d55f65
moni <hugonrosomoni@gmail.com>
2022-03-14 14:19:43 +0100

    slider-petits prix

--- js/sliderpetitsprix.js
+++ js/sliderpetitsprix.js
@@ -9,96 +9,97 @@ class MySection3 extends HTMLElement {
                     <div class="card card-p1">
                     .
                     </div>
-                    <p>Badges - "Se jeter à l'eau..."</p>
-                    <p>5€</p>
+                    <p>Badge<br>Le dindon de la farce</p>
+                    <p><del>5€</del>  <b>3€</b></p>
+                    
                     <a href="">Il me le faut !</a>
                 </div> 
                 <div class="card-prix"> 
                     <div class="card card-p2">
                     .
                     </div>
-                    <p>Boucles d'oreilles - "Mermaid pampilles turquoises"</p>
-                    <p>25€</p>
+                    <p>Boucles d'oreilles<br>Funcky</p>
+                    <p><del>25€</del>  <b>15€</b></p>
                     <a href="">Je les veux !</a>
                 </div> 
                 <div class="card-prix"> 
                     <div class="card card-p3">
                     .
                     </div>
-                    <p>Badges - "Drache queen."</p>
-                    <p>5€</p>
-                    <a href="">Celui-ci, c'est le bon !</a>
+                    <p>Boucles d'oreilles<br>Arc laiton brut</p>
+                    <p><del>25€</del>  <b>15€</b></p>
+                    <a href="">Celles-ci, c'est les bonnes !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p4">
                     .
                     </div>
-                    <p>Badges - "Hi hi. Je jubile."</p>
-                    <p>5€</p>
+                    <p>Badge<br>Mégots, bulots,dodo</p>
+                    <p><del>5€</del>  <b>3€</b></p>
                     <a href="">Je le prends !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p5">
                     .
                     </div>
-                    <p>Badges - "God save the spleen."</p>
-                    <p>5€</p>
+                    <p>Boucles d'oreilles<br>Lilas écailles</p>
+                    <p><del>25€</del>  <b>15€</b></p>
                     <a href="">J'en ai besoin !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p6">
                     .
                     </div>
-                    <p>Badges - "Foutons-nous la paix."</p>
-                    <p>5€</p>
+                    <p>Boucles d'oreilles<br>Trio beige</p>
+                    <p><del>25€</del>  <b>14€</b></p>
                     <a href="">C'est tout-moi !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p7">
                     .
                     </div>
-                    <p>Boucles d'oreilles - "Mosaïc pampilles rouges."</p>
-                    <p>25€</p>
+                    <p>Badge<br>Miss raclette</p>
+                    <p><del>5€</del>  <b>3€</b></p>
                     <a href="">J'achète !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p8">
                     .
                     </div>
-                    <p>Boucles d'oreilles - "Vtamine C"</p>
-                    <p>25€</p>
+                    <p>Boucles d'oreilles<br>blue rainbow</p>
+                    <p><del>25€</del>  <b>15€</b></p>
                     <a href="">Yess... !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p9">
                     .
                     </div>
-                    <p>Cartes - "Vieillir est un privilège."</p>
-                    <p>2€</p>
+                    <p>Badge<br>No Limits but the skai</p>
+                    <p><del>5€</del>  <b>3€</b></p>
                     <a href="">Celui-là, c'est pour moi !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p10">
                     .
                     </div>
-                    <p>Badges - "La reine déneige."</p>
-                    <p>5€</p>
+                    <p>Badge<br>Passionné ne veut pas...</p>
+                    <p><del>5€</del>  <b>3€</b></p>
                     <a href="">J'en rêve !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p11">
                     .
                     </div>
-                    <p>Badges - "Just doux it."</p>
-                    <p>5€</p>
+                    <p>Boucles d'oreilles<br>Noir arc</p>
+                    <p><del>25€</del>  <b>15€</b></p>
                     <a href="">I want it !</a>
                 </div>  
                 <div class="card-prix"> 
                     <div class="card card-p12">
                     .
                     </div>
-                    <p>Cartes - "Distribution de sourires..."</p>
-                    <p>2€</p>
+                    <p>Badge<br> Obsédée textuelle</p>
+                    <p><del>25€</del>  <b>15€</b></p>
                     <a href="">Je passe commande !</a>
                 </div>  
             </div>
