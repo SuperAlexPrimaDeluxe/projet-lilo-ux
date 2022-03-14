38603207189d8716b01bb54493b96e82fb9b0c3c
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-14 13:45:55 +0100

    Test de correction du JS et des chemins
    Test

--- js/pathcorrecter.js
+++ js/pathcorrecter.js
@@ -1,34 +1,33 @@
 /* Appel des îcones et img HEADER */
 
-document.getElementById("logoheader").src = "/projet-lilo-ux/medias/icons/lilo_logo.svg";
-document.getElementById("icon1").src = "/projet-lilo-ux/medias/icons/poeple2.svg"; 
-document.getElementById("icon2").src = "/projet-lilo-ux/medias/icons/basket.svg"; 
-document.getElementById("icon3").src = "/projet-lilo-ux/medias/icons/coeur_lilo.svg"; 
-document.getElementById("burgerheader").src = "/projet-lilo-ux/medias/icons/burger.svg"; 
-document.getElementById("closeheader").src = "/projet-lilo-ux/medias/icons/close.svg"; 
-document.getElementById("lasupericonederecherche").src = "/projet-lilo-ux/medias/icons/search.svg";
-document.getElementById("arrowdown1").src = "/projet-lilo-ux/medias/icons/downarrow.svg";
-document.getElementById("arrowdown2").src = "/projet-lilo-ux/medias/icons/downarrow.svg";
-document.getElementById("searchdesktoop").src = "/projet-lilo-ux/medias/icons/search.svg";
+document.getElementById("logoheader").src = "../medias/icons/lilo_logo.svg";
+document.getElementById("icon1").src = "../medias/icons/poeple2.svg"; 
+document.getElementById("icon2").src = "../medias/icons/basket.svg"; 
+document.getElementById("icon3").src = "../medias/icons/coeur_lilo.svg"; 
+document.getElementById("burgerheader").src = "../medias/icons/burger.svg"; 
+document.getElementById("closeheader").src = "../medias/icons/close.svg"; 
+document.getElementById("lasupericonederecherche").src = "../medias/icons/search.svg";
+document.getElementById("arrowdown1").src = "../medias/icons/downarrow.svg";
+document.getElementById("arrowdown2").src = "../medias/icons/downarrow.svg";
+document.getElementById("searchdesktoop").src = "../medias/icons/search.svg";
 
 /* Appel des îcones et img pour le FOOTER */
 
-document.getElementById("bancontact").src = "/projet-lilo-ux/medias/icons/BANCONTACT_LILO.svg";
-document.getElementById("maestro").src = "/projet-lilo-ux/medias/icons/MAESTRO_LILO.svg";
-document.getElementById("mastercard").src = "/projet-lilo-ux/medias/icons/MASTERCARD_LILO.svg";
-document.getElementById("paypal").src = "/projet-lilo-ux/medias/icons/PAYPAL_LILO.svg";
-document.getElementById("visa").src = "/projet-lilo-ux/medias/icons/VISA_LILO.svg";
-document.getElementById("bpost").src = "/projet-lilo-ux/medias/icons/bpostlogo.svg";
-document.getElementById("logofooterlilo").src = "/projet-lilo-ux/medias/icons/lilologo.svg";
-document.getElementById("facebookfooter").src = "/projet-lilo-ux/medias/icons/facebook.svg";
-document.getElementById("instafooter").src = "/projet-lilo-ux/medias/icons/instagram.svg";
-document.getElementById("mailfooter").src = "/projet-lilo-ux/medias/icons/Mail.svg";
-
+document.getElementById("bancontact").src = "../medias/icons/BANCONTACT_LILO.svg";
+document.getElementById("maestro").src = "../medias/icons/MAESTRO_LILO.svg";
+document.getElementById("mastercard").src = "../medias/icons/MASTERCARD_LILO.svg";
+document.getElementById("paypal").src = "../medias/icons/PAYPAL_LILO.svg";
+document.getElementById("visa").src = "../medias/icons/VISA_LILO.svg";
+document.getElementById("bpost").src = "../medias/icons/bpostlogo.svg";
+document.getElementById("logofooterlilo").src = "../medias/icons/lilologo.svg";
+document.getElementById("facebookfooter").src = "../medias/icons/facebook.svg";
+document.getElementById("instafooter").src = "../medias/icons/instagram.svg";
+document.getElementById("mailfooter").src = "../medias/icons/Mail.svg";
 console.log("la bille noire");
 
 /* Changement des liens pour le HEADER */
 
-document.getElementById("returnhome").href = "/projet-lilo-ux/index.html";
+document.getElementById("returnhome").href = "../index.html";
 document.getElementById("txtconnexion").href = "/projet-lilo-ux/login/connexion.html";
 document.getElementById("godash0").href = "/projet-lilo-ux/login/dashboard.html";
 document.getElementById("godash").href = "/projet-lilo-ux/login/dashboard.html";
