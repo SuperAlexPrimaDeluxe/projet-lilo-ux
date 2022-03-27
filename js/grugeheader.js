bc618a75a8cf0967f35677c5d620ba0a4c9748ff
SuperAlexPrimaDeluxe <95749115+SuperAlexPrimaDeluxe@users.noreply.github.com>
2022-03-27 22:18:59 +0200

    Correction de bug suite

---
+++ js/grugeheader.js
@@ -0,0 +1,5 @@
+document.getElementById("godash0").textContent = "Connexion";
+document.getElementById("psty2").textContent = "0";
+document.getElementById("godash0").href = "login/connexion.html";
+document.getElementById("mopppy1").style.backgroundColor = "#7a7a7a";
+
